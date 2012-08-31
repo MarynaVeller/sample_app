8e55785c78c01489544905be802d059d07fcdd06
MarynaVeller <maryna.veller@gmail.com>
2012-08-31 19:47:32 -0400

    small fix

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "User Pages" do
   
   subject {page}
   describe "Signup Page" do
-    before {visit root_path}
+    before {visit signup_path}
     it {should have_selector 'h1', text: "Sign Up"}
     it {should have_selector 'title', text: full_title("Sign Up")}
   end
