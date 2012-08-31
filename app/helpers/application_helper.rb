6e2adeda26e957a5e8c67469d6d0a28c101350f7
MarynaVeller <maryna.veller@gmail.com>
2012-08-31 19:45:34 -0400

    chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
-	def test
-		"test"
-	end
-		def full_title(page_title)
+
+	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
 			base_title
